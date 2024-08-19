3f32707a8e0148eeebac2fa85047b4ed0f6f0e3b
Irwin <carpool7200@gmail.com>
2024-08-18 21:21:24 -0400

    Ensured api keys are protected

---
+++ src/api/index.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw,ne) => {
+    try{
+        const {data: {data}} = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+            params: {
+              bl_latitude: sw.lat,
+              tr_latitude: ne.lat,
+              bl_longitude: sw.lng,
+              tr_longitude: ne.lng,
+            },
+            headers: {
+              'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+              'x-rapidapi-host': 'travel-advisor.p.rapidapi.com'
+            }
+          });
+
+        return data;    
+    } catch (error){
+        console.log(error);
+    }   
+}
+
+export const getWeatherData = async (lat, lng) => {
+  try{
+      const {data} = await axios.get(`https://open-weather13.p.rapidapi.com/city/latlon/${lat}/${lng}`, {
+          headers: {
+            'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+            'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+          }
+        });
+
+      return data;
+  } catch (error){
+    console.log(error);
+  }
+}
